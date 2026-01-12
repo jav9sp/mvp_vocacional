fbe4d74f97155480f7c6aa262ea50c6b5977d4ca
Francisco Fuentes <150530152+jav9sp@users.noreply.github.com>
2026-01-12 12:42:30 -0300

    added: netlify deploy configurations

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -4,11 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
