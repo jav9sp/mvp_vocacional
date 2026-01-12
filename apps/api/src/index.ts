b0893183d617840f4127120914b7c9da69119a8e
Francisco Fuentes <150530152+jav9sp@users.noreply.github.com>
2026-01-12 14:32:43 -0300

    added: reflect-metadata dependencie

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import "reflect-metadata";
 import { sequelize, connectDB } from "./config/sequelize.js";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger.js";
